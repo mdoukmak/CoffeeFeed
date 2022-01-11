33a54e14dadcfc0283454b2fe2e98b55f43aa7da
Muhammad Doukmak <mkdoukmak@gmail.com>
2022-01-11 11:31:26 -0600

    Initial Commit

---
+++ CoffeeFeed/CoffeeFeed.h
@@ -0,0 +1,18 @@
+//
+//  CoffeeFeed.h
+//  CoffeeFeed
+//
+//  Created by Muhammad Doukmak on 1/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoffeeFeed.
+FOUNDATION_EXPORT double CoffeeFeedVersionNumber;
+
+//! Project version string for CoffeeFeed.
+FOUNDATION_EXPORT const unsigned char CoffeeFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoffeeFeed/PublicHeader.h>
+
+
