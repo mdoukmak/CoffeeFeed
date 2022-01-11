d255e5e7be5a2cf154d18959bd60fd81aa7ba1c9
Muhammad Doukmak <mkdoukmak@gmail.com>
2022-01-11 11:41:03 -0600

    Create CoffeePost model

---
+++ CoffeeFeed/CoffeePost.swift
@@ -0,0 +1,15 @@
+//
+//  CoffeePost.swift
+//  CoffeeFeed
+//
+//  Created by Muhammad Doukmak on 1/11/22.
+//
+
+import Foundation
+
+struct CoffeePost {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
