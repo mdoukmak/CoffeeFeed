cf048421d0e9cfff48f9dd028973a367782b22e6
Muhammad Doukmak <mkdoukmak@gmail.com>
2022-02-21 10:48:17 -0600

    deliversEmptyResult_onHTTP200_withEmptyJSONList

--- CoffeeFeedTests/RemoteCoffeePostsLoaderTests.swift
+++ CoffeeFeedTests/RemoteCoffeePostsLoaderTests.swift
@@ -64,6 +64,23 @@ class RemoteCoffeePostsLoaderTests: XCTestCase {
             client.complete(withStatusCode: 200, data: invalidJSON)
         }
     }
+    
+    func test_load_deliversEmptyResult_onHTTP200_withEmptyJSONList() {
+        let (sut, client) = makeSUT()
+        
+        let emptyListJSON = Data("{\"items\":[]}".utf8)
+        var capturedResults = [RemoteCoffeePostsLoader.Result]()
+        
+        sut.load { result in
+            capturedResults.append(result)
+        }
+        
+        client.complete(withStatusCode: 200, data: emptyListJSON)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+    }
+    
+    // MARK: - Helpers
 
     private func makeSUT(url: URL = URL(string: "https://any-url.com")!) -> (RemoteCoffeePostsLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
