306b3e9774a35ef8691feac9cb91f6773221455f
Muhammad Doukmak <mkdoukmak@gmail.com>
2022-02-21 23:12:25 -0600

    Finish URL loading when observing requests to make sure all URL requests are finished before the test method returns.

--- CoffeeFeedTests/CoffeeFeedAPI/URLSessionHTTPClientTests.swift
+++ CoffeeFeedTests/CoffeeFeedAPI/URLSessionHTTPClientTests.swift
@@ -181,6 +181,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func startLoading() {
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return requestObserver(request)
+            }
+            
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
